a035eb3556724fb6d8177b94a039854eb2245991
d0ndar <popov.m14a@gmail.com>
2024-10-24 21:17:41 +0500

    new branch for lab 1

--- obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c6774b6ccdb3452a65f4d64cc1e3ad95889cb6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5052667dbf9c2a19242ecbdaaa6fd68e85defe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
